987593ab67476546023703d232ce8a7b7469ec91
awerp <johnsondouglas918@gmail.com>
2016-04-14 14:24:51 -0600

    A completed Datalog Interpreter written in C++

---
+++ expression.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"Parameter.h"
+#include<list>
+#include<sstream>
+class expression
+{
+public:
+	expression(Parameter first,string op,Parameter second);
+	~expression();
+	string toString();
+private:
+	Parameter one, two;
+	string oper;
+};
+
